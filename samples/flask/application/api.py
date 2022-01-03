24a4fc6b85452d3efd445598e7ecaf1112aaf745
Ricardo Patiño <rickypj3092@gmail.com>
2022-01-03 04:34:18 -0500

    Added Flask sample

---
+++ samples/flask/application/api.py
@@ -0,0 +1,28 @@
+from csv import DictReader
+from flask import current_app as app, request, abort
+from flask.json import dumps
+import requests
+
+@app.route("/api/data/")
+def get_data():
+    FILE_NAME = "test.csv"
+
+    url = request.args.get("url", type=str)
+
+    if url:
+        try:
+            with requests.Session() as session:
+                stream = session.get(url)
+                file = stream.content.decode("utf-8")
+                reader = DictReader(file)
+                json = dumps([row for row in reader], indent=4)
+                return json
+
+        except Exception as e:
+            print(e.args)
+            return abort(500)
+
+    with open(FILE_NAME, encoding="utf-8") as file:
+        reader = DictReader(file)
+        json = dumps([row for row in reader], indent=4)
+        return json
